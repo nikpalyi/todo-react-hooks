51467049bcc13a88b34cfeaecf6e46f30e1fe0a3
nikpalyi <diavetito@gmail.com>
2019-09-11 21:24:36 +0100

    ToDo List component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import './styles.css';
 
+import TodoForm from './TodoForm';
+import React, { useState } from 'react';
+
 const App = () => {
+  const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-      <Typography component="h1" variant="h2">
+    <div className='App'>
+      <Typography component='h1' variant='h2'>
         Todos
       </Typography>
+      <TodoForm saveTodo={console.warn} />
     </div>
   );
 };
