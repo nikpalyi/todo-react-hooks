33df5678be96b2699577e44a31face4f9757708f
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 22:04:19 +0000

    deploy

---
+++ build/precache-manifest.de628e74afe1821370ead1aa4031e836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "329a1fac7a6467035dd6",
+    "url": "/todo-react-hooks/static/js/runtime~main.329a1fac.js"
+  },
+  {
+    "revision": "25aa788b3622a69b8fbb",
+    "url": "/todo-react-hooks/static/js/main.25aa788b.chunk.js"
+  },
+  {
+    "revision": "c5e1b5f52d447b124c17",
+    "url": "/todo-react-hooks/static/js/1.c5e1b5f5.chunk.js"
+  },
+  {
+    "revision": "25aa788b3622a69b8fbb",
+    "url": "/todo-react-hooks/static/css/main.3a5f29d9.chunk.css"
+  },
+  {
+    "revision": "3f6dde1af9baab5d4946832f5a124060",
+    "url": "/todo-react-hooks/index.html"
+  }
+];
